1f75cf4b73e754fdf21290cd55c467b3cfc671bc
rajesh <arkadrajesh@gmail.com>
2022-07-05 09:18:11 +0530

    button added

--- src/components/effect/user.jsx
+++ src/components/effect/user.jsx
@@ -4,19 +4,25 @@ import axios from 'axios';
 const User = (props) => {
 	const [ id, setId ] = useState(1);
 	const [ post, setPost ] = useState([]);
+    const [idFromButtonClick,setIdFromButtonClick]=useState(id)
 
 	useEffect(() => {
 		getUser();
-	},[id]);
+	},[idFromButtonClick]);
+
+    const handleClick=()=>{
+        setIdFromButtonClick(id);
+    }
 
 	async function getUser() {
-		const result = await axios(`https://jsonplaceholder.typicode.com/posts/${id}`);
+		const result = await axios(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`);
 		console.log(result);
 		setPost(result.data);
 	}
 	return (
 		<div>
 			<input type="text" value={id} onChange={(e) => setId(e.target.value)}></input>
+            <button type='button' onClick={handleClick}>fetch</button>
 			<h5>{post.title}</h5>
 		</div>
 	);
